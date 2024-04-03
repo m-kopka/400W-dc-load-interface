260f6f52bee10665fbe16f0e259827503c455c02
m-kopka <m.kopka@post.cz>
2024-04-03 13:53:47 +0200

    add: clock definitions

--- src/main.c
+++ src/main.c
@@ -11,17 +11,21 @@ int main() {
 
     // enable the crystal oscillator, initialize the PLL and set the system clock source to PLL without glitching the system clock
     xosc_enable();
-    clocks_set_source(clk_sys, CLOCK_SRC_ROSC_CLKSRC_PH);
+    clocks_set_source(clk_sys, CLOCK_SRC_SYS_CLK_REF);
     pll_init(PLL_SYS, 100, 6, 2);
-    clocks_set_aux_source(clk_sys, CLOCK_AUXSRC_CLKSRC_PLL_SYS);
-    clocks_set_source(clk_sys, CLOCK_SRC_CLKSRC_CLK_REF_AUX);
+    clocks_set_aux_source(clk_sys, CLOCK_AUXSRC_SYS_CLKSRC_PLL_SYS);
+    clocks_set_source(clk_sys, CLOCK_SRC_SYS_CLKSRC_CLK_SYS_AUX);
 
     // set the XOSC as a reference clock and initialize the frequency counter
-    clocks_set_source(clk_ref, CLOCK_SRC_XOSC_CLKSRC);
+    clocks_set_source(clk_ref, CLOCK_SRC_REF_XOSC_CLKSRC);
     fc0_init(F_XOSC_HZ);
 
     // initialize the watchdog timer to generate a 1us tick for the timer
     watchdog_start_tick(F_XOSC_HZ);
+
+    // configure the peripheral clock
+    clocks_set_aux_source(clk_peri, CLOCK_AUXSRC_PERI_CLK_SYS);
+    clocks_set_enable(clk_peri, true);
 
     gpio_init();
 
